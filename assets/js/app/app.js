a28efbb15f96a6d787355aa9f34e3fa7bf283df5
israjHaliri <israj.haliri@gmail.com>
2016-08-23 20:41:40 +0700

    add route done

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -2,8 +2,6 @@ var app = angular.module('myApp', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/', {templateUrl: 'pages/list.html', controller: 'contentController'}).
-      when('/add', {templateUrl: 'pages/add.html', controller: 'contentController'}).
-      when('/edit/:id', {templateUrl: 'pages/edit.html', controller: 'contentController'}).
+      when('/', {templateUrl: 'pages/content.html', controller: 'contentController'}).
       otherwise({redirectTo: '/'});
 }]);
--- assets/js/app/controller/content-controller.js
+++ assets/js/app/controller/content-controller.js
@@ -2,6 +2,7 @@ app.controller('contentController', function($scope, $http, $location, $routePar
 
    var id = $routeParams.id;
    $scope.active_path = null;
+   $scope.category = "save"; 
 
 
    $scope.options = [
@@ -17,9 +18,9 @@ app.controller('contentController', function($scope, $http, $location, $routePar
 
    $scope.active = $scope.options[0];
 
-   $scope.loadDataUser= function () {
+   $scope.loadData= function () {
       $http({
-         url: 'http://localhost:8080/listData2',
+         url: 'http://localhost:8080/api/list_data',
          dataType: 'json',
          method: 'GET',
          data: '',
@@ -35,39 +36,85 @@ app.controller('contentController', function($scope, $http, $location, $routePar
       });
    };
 
-   $scope.processDataUser= function () {
 
-      $http.post('http://localhost:8080/insert_model', {
+
+   $scope.processData= function (param) {
+
+      console.log("category=",param);
+
+      if (param == "save") 
+      {
+         $scope.saveData();
+      }
+      else
+      {
+         $scope.updateData();
+      }
+
+   };
+
+
+   $scope.saveData= function () {
+
+      $http.post('http://localhost:8080/api/insert', {
+         'username' : $scope.username,
+         'password' : $scope.password, 
+         'active' : $scope.active.value
+      }).success(function (response){             
+         console.log("response save =",response);
+         $scope.loadData();
+         $scope.clear();
+      }, function myError(response) {
+         console.log("response save =",response);
+      });
+   };
+
+   $scope.updateData= function () {
+
+      $http.put('http://localhost:8080/api/update', {
          'id' : $scope.id,
          'username' : $scope.username,
          'password' : $scope.password, 
          'active' : $scope.active.value
       }).success(function (response){             
          console.log("response save =",response);
-         $scope.active_path = $location.path('/');
+         $scope.loadData();
+         $scope.clear();
       }, function myError(response) {
          console.log("response save =",response);
       });
    };
 
-   $scope.editDataUser= function () {
+   $scope.editData= function (param) {
 
-      $http.get('http://localhost:8080/listData2/'+id).success(function(response) {
-         $scope.listData = response.data;
-
+      $http.get('http://localhost:8080/api/list_data/'+param).success(function(response) {
          $scope.id = response.data[0]["id"];
-         // $scope.username = response.data[0]["username"];
-         // $scope.password = response.data[0]["password"];
-         
+         $scope.username = response.data[0]["username"];
+         $scope.password = response.data[0]["password"];  
+         $scope.active = $scope.options[0];  
+         $scope.category = "edit";      
       });
 
    };
 
-   $scope.deleteDataUser = function (param) {
+   $scope.deleteData = function (param) {
       if(confirm("Are you sure to delete this data?")){
-         $http.get("http://localhost:8080/delete/"+param).success(function(response){
-            $scope.loadDataUser();
+         $http.get("http://localhost:8080/api/delete/"+param).success(function(response){
+            $scope.loadData();
          });
       }
    };
+
+   $scope.clear= function (param) {
+
+      $http.get('http://localhost:8080/api/list_data/'+param).success(function(response) {
+         $scope.id = "";
+         $scope.username = "";
+         $scope.password = "";
+         $scope.active = "";      
+         $scope.category = "save";      
+      });
+
+   };
+
 });
